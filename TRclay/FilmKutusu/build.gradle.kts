a9ee4297a3435233216caef187334605ea0c3bd1
trclay34 <trclay34@gmail.com>
2026-05-20 11:50:19 +0300

    Update build.gradle.kts

--- TRclay/FilmKutusu/build.gradle.kts
+++ TRclay/FilmKutusu/build.gradle.kts
@@ -1,1 +1,12 @@
+apply plugin: "com.android.library"
+apply plugin: "kotlin-android"
+apply plugin: "com.lagradost.cloudstream3.gradle"
 
+cloudstream {
+    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "trclay34/TRclay")
+    authors = listOf("trclay34")
+    description = "FilmKutusu.com.tr Eklentisi"
+    language = "tr"
+    tvTypes = listOf("Movie")
+    iconUrl = "https://filmkutusu.com.tr/wp-content/uploads/2026/03/filmkutusu-logo.png"
+}
