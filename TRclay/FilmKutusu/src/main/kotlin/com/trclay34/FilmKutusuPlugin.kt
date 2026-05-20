8121a10fbe8d5b1becc93fcbfd3b09957350600f
trclay34 <trclay34@gmail.com>
2026-05-20 11:49:43 +0300

    Update FilmKutusuPlugin.kt

--- TRclay/FilmKutusu/src/main/kotlin/com/trclay34/FilmKutusuPlugin.kt
+++ TRclay/FilmKutusu/src/main/kotlin/com/trclay34/FilmKutusuPlugin.kt
@@ -1,1 +1,13 @@
+package com.trclay34
 
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.APIHolder
+
+@CloudstreamPlugin
+class FilmKutusuPlugin: Plugin() {
+    override fun load(context: Context) {
+        APIHolder.allProviders.add(FilmKutusu())
+    }
+}
